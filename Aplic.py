c03a8baba157bbbd381002ca3ebf15a714994497
luanferreira2 <luan.silva115@fatec.sp.gov.br>
2025-05-02 12:10:52 -0300

    Retangulo

---
+++ Aplic.py
@@ -0,0 +1,13 @@
+from classe import Retangulo
+
+base = float(input(' Digite a base do retangulo: '))
+altura = float(input('Digite a altura do retangulo: '))
+
+retangulo1 = Retangulo(base, altura)
+
+print(f' A base do retangulo é de: {retangulo1.getBase()}')
+print(f'A altura do retangulo é de: {retangulo1.getAltura()}')
+
+print(f' A área do retangulo é de: {retangulo1.calcArea()}')
+print(f' O perímetro do retangulo é de: {retangulo1.calcPerimetro()}')
+print(f' A diagonal do retangulo é de: {retangulo1.calcDiagonal()}')
